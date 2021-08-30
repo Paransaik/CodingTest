0bbb2deef0213fbdfe4dec35a97ab37eae482e7a
Paransaik <30463982+Paransaik@users.noreply.github.com>
2021-08-31 08:40:03 +0900

    8.31. clear 2_dart_game.py

--- 2_dart_game.py
+++ 2_dart_game.py
@@ -2,27 +2,28 @@ from typing import *
 # 8.30. am. 21:30~
 
 def dart_game(arr1: List[int]) -> int:
-    result: int = 0
-    current: List[int] = [0]
+    num: List[int] = [0]
 
     for s in arr1:
-        # if s == 'S':
-        #     pass
-
-        if s == 'D':
+        if s == 'S':
+            num.append(0)
             pass
+        elif s == 'D':
+            num[-1] = num[-1] ** 2
+            num.append(0)
+        elif s == 'T':
+            num[-1] = num[-1] ** 3
+            num.append(0)
+        elif s == '*':
+            num[-2] *= 2
+            if len(num) > 2:
+                num[-3] *= 2
+        elif s == '#':
+            num[-1] *= -1
+        else:
+            num[-1] = num[-1] * 10 + int(s)
 
-        if s == 'T':
-            pass
-
-        if s == '*':
-            result -= current[-1]
-            result += current[-1] * 2
-            current.append(current[-1] * 2)
-
-        if s == '#':
-            pass
-
-    return result
+        print(num)
+    return sum(num)
 
 print('result', dart_game('1S2D*3T'))
