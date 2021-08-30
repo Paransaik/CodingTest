024ed641e8589f4c05b432fef4f0fc422e61b5c4
Paransaik <30463982+Paransaik@users.noreply.github.com>
2021-08-31 08:46:14 +0900

    8.31. add 2_dart_game.py dataset

--- 2_dart_game.py
+++ 2_dart_game.py
@@ -19,11 +19,16 @@ def dart_game(arr1: List[int]) -> int:
             if len(num) > 2:
                 num[-3] *= 2
         elif s == '#':
-            num[-1] *= -1
+            num[-2] *= -1
         else:
             num[-1] = num[-1] * 10 + int(s)
-
-        print(num)
+        # print(num)
     return sum(num)
 
-print('result', dart_game('1S2D*3T'))
+print('result', dart_game('1S2D*3T'))  # 27
+print('result', dart_game('1D2S#10S'))  # 9
+print('result', dart_game('1D2S0T'))  # 3
+print('result', dart_game('1S*2T*3S'))  # 23
+print('result', dart_game('1D#2S*3S'))  # 5
+print('result', dart_game('1T2D3D#'))  # -4
+print('result', dart_game('1D2S3T*'))  # 59
