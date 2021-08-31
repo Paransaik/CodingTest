b2e4dd9922f501fa949dfa36733dae4c5feb6fbd
Paransaik <30463982+Paransaik@users.noreply.github.com>
2021-08-31 11:30:20 +0900

    8.31. add 4_shuttle_bus.py

---
+++ 4_shuttle_bus.py
@@ -0,0 +1,9 @@
+from typing import *
+# 8.31. am. 11:00~12:00
+
+def shuttle_bus(n: int, t: int, m: int, timetable: List[str]) -> str:
+    time: str
+
+    return time
+
+print('result', shuttle_bus(n, t, m, timetalbe)
