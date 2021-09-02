ce8c8fb588f7d7a2985d6062454ac3f65aa5f3f2
Paransaik <jty419@naver.com>
2021-09-02 18:27:10 +0900

    9.2.

--- 5_news_clustering.py
+++ 5_news_clustering.py
@@ -1,9 +1,35 @@
-from typing import *
-# .
+# from typing import *
+# # .
+#
+# def news_clustering(arr1: List[str], arr2: List[str]) -> int:
+#     time: str
+#
+#     return time
+#
+# print('result', news_clustering('FRANCE', 'french'))
 
-def news_clustering(arr1: List[str], arr2: List[str]) -> int:
-    time: str
 
-    return time
+def solution(numbers, target):
+    answer = 0
+    result = []
+    prev_emulate = []
 
-print('result', news_clustering('FRANCE', 'french'))
+    def dfs(nums):
+        # 종료 조건
+        if len(nums) == 0:
+            result.append(prev_emulate[:])
+
+        # 반복 조건
+        for e in nums:
+            next_emulate = nums[:]
+            next_emulate.remove(e)
+
+            prev_emulate.append(e)
+            dfs(next_emulate)
+            prev_emulate.pop()
+
+    dfs(numbers)
+    print(result)
+    return answer
+
+print('result', solution([1,2,3,4,5], 3))
