b4e0e7c1ceb7948803736defdf9566bc79cc86a9
Paransaik <jty419@naver.com>
2021-09-03 04:33:47 +0900

    9.3. add solution.py

--- 5_news_clustering.py
+++ 5_news_clustering.py
@@ -9,27 +9,14 @@
 # print('result', news_clustering('FRANCE', 'french'))
 
 
-def solution(numbers, target):
-    answer = 0
-    result = []
-    prev_emulate = []
+#
+def solution(begin, target, words):
+    if begin == []:
+        if target == 'cog':
+            return 1
+        else:
+            return 0
+    else:
+        return solution(numbers[1:], target+numbers[0]) + solution(numbers[1:], target-numbers[0])
 
-    def dfs(nums):
-        # 종료 조건
-        if len(nums) == 0:
-            result.append(prev_emulate[:])
-
-        # 반복 조건
-        for e in nums:
-            next_emulate = nums[:]
-            next_emulate.remove(e)
-
-            prev_emulate.append(e)
-            dfs(next_emulate)
-            prev_emulate.pop()
-
-    dfs(numbers)
-    print(result)
-    return answer
-
-print('result', solution([1,2,3,4,5], 3))
+print(solution([1,1,1,1,1], 3))
---
+++ solution.py
@@ -0,0 +1,16 @@
+def solution(begin, target, words):
+    answer = 0
+
+    def dfs(word, currnt):
+        if currnt == 'target':
+            return answer
+        else:
+            return 0
+
+        for i in range(len(begin))
+            hegin
+            for
+
+                answer += 1
+
+    dfs(beign, '')
