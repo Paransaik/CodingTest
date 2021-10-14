6c2c117a338e0fe463aa7b5f3f0b5d7a7d759dbf
Paransaik <30463982+Paransaik@users.noreply.github.com>
2021-10-14 18:34:00 +0900

    10.14. M BOJ 1662 중

--- solution.py
+++ solution.py
@@ -1,29 +1,28 @@
 # BOJ 1662
-a = "33(562(71(9)))"
-result = ""
-word = ""
-# 스택
-board = a.split('(')
-# board = board[:-1] + board[-1].split(')')
+# import sys
+# print(sys.getsizeof(9))
+# print(sys.getsizeof("9"))
+
+# a = input()
+a = "15(22)13(92(1111)42(222))" #60
+# a = "15(22)13(92(1111)42(222))123(1)45" #67
+stack = []
+
+board = a.split('(' or ')')
 print(board)
-# def recursive(S):
-#     if len(S) == 0:
-#         return result
-#
-#     for i, s in enumerate(board):
-#         if s == '(':
-#             # push [:i-1]
-#             Str = board[i+1:]
-#
-#         elif s == ')':
-#             # pop = l
-#             # result = l[:-1] + l[-1] * board[:i-1]
-#             # return result
-#             pass
-#
-#         word = word + s
-#
-# print(recursive(a))
+for i in range(len(board)-1):
+    stack.append(board[i])
+print(stack)
+
+board = board[-1].split(')')
+result = len(board[0])
+
+for i in range(1, len(board)):
+    last_word = str(stack.pop())
+    print(last_word, len(last_word[:-1]), result, int(last_word[-1]), len(board[i]))
+    result = len(last_word[:-1]) + result * int(last_word[-1]) + len(board[i])
+
+print(result)
 
 #66	141	53.226%
 #67	142	53.600%
