0ef8231c3d0415852866381e56efc916190c37be
Paransaik <jty419@naver.com>
2021-10-14 15:53:21 +0900

    10.14. BOJ 1662 中2

--- solution.py
+++ solution.py
@@ -1,24 +1,29 @@
 # BOJ 1662
 a = "33(562(71(9)))"
 result = ""
+word = ""
 # 스택
-board = list(a)
-
-def recus(S):
-    if len(S) == 0:
-        return result
-
-    for i, s in enumerate(board):
-        if s == '(':
-            # push [:i-1]
-            Str = board[i+1:]
-            recus(Str)
-        elif s == ')':
-            # pop = l
-            # result = l[:-1] + l[-1] * board[:i-1]
-            return result
-
-print(recus(a))
+board = a.split('(')
+# board = board[:-1] + board[-1].split(')')
+print(board)
+# def recursive(S):
+#     if len(S) == 0:
+#         return result
+#
+#     for i, s in enumerate(board):
+#         if s == '(':
+#             # push [:i-1]
+#             Str = board[i+1:]
+#
+#         elif s == ')':
+#             # pop = l
+#             # result = l[:-1] + l[-1] * board[:i-1]
+#             # return result
+#             pass
+#
+#         word = word + s
+#
+# print(recursive(a))
 
 #66	141	53.226%
 #67	142	53.600%
