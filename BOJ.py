2973655de7a585395369878682c3b265c54066a2
Paransaik <30463982+Paransaik@users.noreply.github.com>
2021-12-17 03:39:19 +0900

    12.17. 1662

--- BOJ.py
+++ BOJ.py
@@ -1,33 +1,29 @@
-# # BOJ 1662 fail
-# # import sys
-# # print(sys.getsizeof(9))
-# # print(sys.getsizeof("9"))
-#
-# # a = input()
-# # a = "33(562(71(9)))"
-# a = "15(22)13(92(1111)42(222))" #60
-# # a = "15(22)13(92(1111)42(222))123(1)45" #67
-# stack = []
-#
-# board = a.split('(')
-#
-# for i in range(len(board)):
-#     stack.append(board[i])
-# # print(stack)
-#
-# board = stack.pop().split(')')
-# print(stack)
-# print(board)
-# result = len(board[0])
-#
-# for i in range(1, len(board)):
-#     last_word = str(stack.pop())
-#     # print(last_word, len(last_word[:-1]), result, int(last_word[-1]), len(board[i]))
-#     result = len(last_word[:-1]) + result * int(last_word[-1]) + len(board[i])
-#
-# print(result)
-#
-# #66	141	53.226%
-# #67	142	53.600%
-# #67	143	53.600%
+# BOJ 1662 fail
+# import sys
+# print(sys.getsizeof(9))
+# print(sys.getsizeof("9"))
+
+# a = input()
+# a = "33(562(71(9)))"
+a = "15(22)13(92(1111)42(222))" #60
+# a = "15(22)13"
+# a = "15(22)13(92(1111)42(222))123(1)45" #67
+stack = []
+board = a.split(')')
+print(board)
+
+for string in board:
+    stack.append(string)
+
+#     if c == ')':
+#         word = stack.pop()
+#         pre = word[:-1]
+#         inor = word[-1]
+#         post = string[pointer+1:]
+#         q = string[:pointer]
+#         stack.append(pre + int(inor) * q + post)
+#     else:
+#         pointer += 1
+#     else:  # )가 없으면
+#         stack.append(string)
 
