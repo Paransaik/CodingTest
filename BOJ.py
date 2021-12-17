af8c144a661fc65e3cfdfbf44c9649e6a0725954
Paransaik <jty419@naver.com>
2021-12-17 15:57:09 +0900

    12.17. 1662 ~ing

--- BOJ.py
+++ BOJ.py
@@ -1,29 +1,30 @@
 # BOJ 1662 fail
-# import sys
-# print(sys.getsizeof(9))
-# print(sys.getsizeof("9"))
 
-# a = input()
-# a = "33(562(71(9)))"
 a = "15(22)13(92(1111)42(222))" #60
-# a = "15(22)13"
 # a = "15(22)13(92(1111)42(222))123(1)45" #67
-stack = []
-board = a.split(')')
-print(board)
 
+a = input()
+board = a.split('(')
+stack = []
 for string in board:
     stack.append(string)
 
-#     if c == ')':
-#         word = stack.pop()
-#         pre = word[:-1]
-#         inor = word[-1]
-#         post = string[pointer+1:]
-#         q = string[:pointer]
-#         stack.append(pre + int(inor) * q + post)
-#     else:
-#         pointer += 1
-#     else:  # )가 없으면
-#         stack.append(string)
+while len(stack) != 1:
+    if stack[-1].find(')') > 0:
+        word1 = stack.pop()
+        cursor = 0
+        for i in range(len(word1)):
+            if word1[cursor] == ')':
+                b = word1[:cursor]
+                print(b)
+                print(stack)
+                break
+            cursor += 1
 
+        word2 = stack.pop()
+        pre = word2[:-1]
+        post = int(word2[-1])
+        stack.append(pre + ((post * b) + word1[cursor+1:]))
+        print(stack)
+
+print(len(stack[0]))
