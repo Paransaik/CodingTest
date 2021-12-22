5273431aee49e54fcf23c4ef8d25e76b9cde97d1
Paransaik <jty419@naver.com>
2021-12-22 22:45:13 +0900

    12.22. 15651 sol

--- BOJ.py
+++ BOJ.py
@@ -1,74 +1,78 @@
-# # BOJ 1662 fail
-#
-# a = "15(22)13(92(1111)42(222))" # 60
-# # a = "15(22)13(92(1111)42(222))123(1)45" #67
-#
-# a = input()
+# BOJ 1662 fail
+import sys
+# a = sys.stdin.readline()
+# a = "33(562(71(9)))"
+# a = "3(3(3(2(2)2(2))))"  # 108
+# a = "9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(9(111))))))))))))))))))))))))"
+# a = "123(3)"
+# a = "10342(76)"
+# a = "0(0)"
 # board = a.split('(')
 # stack = []
+# intstack = []
+#
 # for string in board:
 #     stack.append(string)
 #
+# if stack[-1].find(')') > -1:
+#     word1 = stack.pop()
+#     cursor = 0
+#     for i in range(len(word1)):
+#         if word1[cursor] == ')':
+#             b = len(word1[:cursor])
+#             break
+#         cursor += 1
+#     word2 = stack.pop()
+#     pre = len(word2[:-1])
+#     post = int(word2[-1])
+#     stack.append(pre + str(post * b) + word1[cursor+1:])
+#
 # while len(stack) != 1:
-#     if stack[-1].find(')') > 0:
+#     if stack[-1].find(')') > -1:
 #         word1 = stack.pop()
 #         cursor = 0
 #         for i in range(len(word1)):
 #             if word1[cursor] == ')':
-#                 b = word1[:cursor]
-#                 print(b)
-#                 print(stack)
+#                 b = int(word1[:cursor])
 #                 break
 #             cursor += 1
-#
 #         word2 = stack.pop()
-#         pre = word2[:-1]
+#         pre = len(word2[:-1])
 #         post = int(word2[-1])
-#         stack.append(pre + ((post * b) + word1[cursor+1:]))
+#         stack.append(str(pre + (post * b)) + word1[cursor+1:])
 #         print(stack)
 #
-# print(len(stack[0]))
+#
+# print(stack[0])
+
+# 15651
+
+a, index = map(int, input().split())
+result = []
+
+def dfs(elements, index: int):
+    if index == 0:
+        result.append(elements[:])
+        return
+
+    for i in range(1, a + 1):
+        elements.append(i)
+        dfs(elements, index - 1)
+        elements.pop()
+
+dfs([], index)
+for i in result:
+    print(i)
 
 
-# BOJ 17829
-import sys
+# for i in result:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
 
-a = int(input())
-board = []
-for i in range(a):
-    board.append(list(map(int, input().split())))
-
-def CNN(board, a):
-    row = []
-    for i in range(0, a, 2):
-        col = []
-        for j in range(0, a, 2):
-            sort_array = [board[i][j], board[i][j+1], board[i+1][j], board[i+1][j+1]]
-            sort_array.sort()
-            col.append(sort_array[2])
-        row.append(col)
-    if a == 2:
-        return row
-
-    return CNN(row, a//2)
-
-print(CNN(board, a)[0][0])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# for i in result:
+#     if i == sorted(i):
+#         print(*i)
 
 
 
