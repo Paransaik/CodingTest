41f0d98b2c8dce5de8f4fc3ce31dea8804528a90
정태영 <taeyeong.jeong419@gmail.com>
2022-02-12 06:25:29 +0900

    2.12. BOJ 2605 solved

---
+++ Eclipse/Tip.java
@@ -0,0 +1,18 @@
+package Eclipse;
+
+public class Tip {
+
+////	배열 합 구하기
+//	IntStream.of(배열).sum()
+//	lst.stream().mapToInt(Integer::intValue).sum()
+	
+////	StringBuileder 주요 메소드	
+//	StringBuilder append(String s) : StringBuilder 뒤에 값을 붙임
+//	StringBuilder delete(int start , int end) : 특정 인덱스부터 인덱스까지를 삭제
+//	StringBuilder insert(int offet, any primitive of a char[]) : 문자를 삽입함
+//	StringBuilder replace(int start , int end , String s) : 일부를 String 객체로 치환
+//	StringBuilder reverse() : 순서를 뒤집음
+//	StringBuilder setCharAt(int index , char ch) : 주어진 문자를 치환
+//	StringBuilder indexOf(String s) : 값이 어느 인덱스에 들어있는지 확인
+//	StringBuilder subString(int start, int end) : start와 end 사이의 값을 잘라옴.
+}
