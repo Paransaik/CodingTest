3c8b8760a46dc464114cd5062c32a53ee4b97f06
정태영 <taeyeong.jeong419@gmail.com>
2022-02-21 20:27:31 +0900

    2.21. BOJ 24082 solved

---
+++ Eclipse/BOJ_B5_24082_立方体.java
@@ -0,0 +1,13 @@
+package Eclipse;
+import java.util.Scanner;
+
+public class BOJ_B5_24082_立方体 {
+	static int N;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		System.out.println(N*N*N);
+	}
+
+}
--- Eclipse/Form.java
+++ Eclipse/Form.java
@@ -1,27 +1,21 @@
+package Eclipse;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Form {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-
-	static int N;
-	static int[] arr;
+	static int N, P, E;
+	static int arr[];
 	
 	public static void main(String[] args) throws Exception {
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i] = Integer.parseInt(st.nextToken());
-			
-			
-		}
 	}
 
 }
