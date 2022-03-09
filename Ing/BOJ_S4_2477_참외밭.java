2e10aa2cdf23c9671ca4db1be93e7359ff6729da
Paransaik <jty419@naver.com>
2022-03-09 18:48:34 +0900

    3.9. BOJ 1717 solved

---
+++ Ing/BOJ_S4_2477_참외밭.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_S4_2477_참외밭 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static StringTokenizer st;
+
+	static int K, d, distance;
+	static int[][] disArr;
+
+	public static void main(String[] args) throws Exception {
+		K = Integer.parseInt(br.readLine());
+		disArr = new int[6][2];
+
+		for (int i = 0; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				disArr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		System.out.println(Arrays.deepToString(disArr));
+	}
+
+}
---
+++ IntelliJ/BOJ_G4_1717_집합의표현.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_1717_집합의표현 {
+    static int N, M;
+    static int[] parents;
+
+    public static void makeSet() {
+        parents = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parents[i] = i;
+        }
+    }
+
+    public static int findSet(int a) {
+        if (a == parents[a]) return a;
+        return parents[a] = findSet(parents[a]);
+    }
+
+    public static boolean union(int a, int b) {
+        int aRoot = findSet(a);
+        int bRoot = findSet(b);
+        if (aRoot == bRoot) return false;
+        if (aRoot > bRoot) parents[aRoot] = bRoot;
+        else parents[bRoot] = aRoot;
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        makeSet();
+
+        int oper, to, from;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            oper = Integer.parseInt(st.nextToken());
+            to = Integer.parseInt(st.nextToken());
+            from = Integer.parseInt(st.nextToken());
+            if (oper == 0) union(to, from);
+            else {
+                if (findSet(to) == findSet(from)) bw.append("YES").append("\n");
+                else bw.append("NO").append("\n");
+            }
+        }
+        bw.flush();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
