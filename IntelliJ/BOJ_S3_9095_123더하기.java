7b4b13813a8dca4de7aa0dc0a72ffbf73e7bfbb1
Paransaik <jty419@naver.com>
2022-03-12 17:04:40 +0900

    3.12. BOJ 17070 solved

---
+++ IntelliJ/BOJ_S3_9095_123더하기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_9095_123더하기 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] numbers = new int[11];
+        int N = Integer.parseInt(br.readLine());
+
+    }
+
+
+}
