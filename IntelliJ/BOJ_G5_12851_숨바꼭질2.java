e859b71484c328f8654953314d867f357f040515
Paransaik <jty419@naver.com>
2022-03-20 13:23:51 +0900

    3.20. BOJ 12851 solved

--- IntelliJ/BOJ_G5_12851_숨바꼭질2.java
+++ IntelliJ/BOJ_G5_12851_숨바꼭질2.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class BOJ_G5_12851_숨바꼭질2 {
     static int N, K, ans = Integer.MAX_VALUE, method = 0, num;
-    static boolean[] visited;
+    static final int MAX = 100000;
+    static boolean[] visited = new boolean[MAX + 1];
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         K = sc.nextInt();
         num = Math.max(N, K);
-        visited = new boolean[num + 2];
-        if (N != K) bfs(N);
-        else ans = 0;
+        bfs(N);
         System.out.println(ans);
         System.out.println(method);
     }
@@ -28,21 +27,20 @@ public class BOJ_G5_12851_숨바꼭질2 {
             position = q.poll();
             pos = position[0];
             idx = position[1];
+            visited[pos] = true;
+
             if (pos == K && ans >= idx) {
                 ans = Math.min(idx, ans);
                 method++;
             }
             if (pos <= num - 1 && !visited[pos + 1]) {
                 q.offer(new int[] { pos + 1, idx + 1 });
-                if (pos + 1 != K)  visited[pos + 1] = true;
             }
             if (pos <= (num + 1) / 2 && !visited[pos * 2]) {
                 q.offer(new int[] { pos * 2, idx + 1 });
-                if (pos * 2 != K) visited[pos * 2] = true;
             }
             if (pos > 0 && pos <= num + 1 && !visited[pos - 1]) {
                 q.offer(new int[] { pos - 1, idx + 1 });
-                if (pos - 1 != K) visited[pos - 1] = true;
             }
         }
     }
