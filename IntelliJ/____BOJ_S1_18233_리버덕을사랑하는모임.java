ee403b8dcfd26dc000eb8b964b27ca86f38ee5f2
Paransaik <jty419@naver.com>
2022-03-23 03:03:14 +0900

    3.23. move ing --> IntelliJ

---
+++ IntelliJ/____BOJ_S1_18233_리버덕을사랑하는모임.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_S1_18233_리버덕을사랑하는모임 {
+	static int N, P, E;
+	static long arr[];
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		arr = new long[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i] |= (1 << Integer.parseInt(st.nextToken()));				
+			}
+		}
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+	}
+}
+
---
+++ IntelliJ/____BOJ_S2_괄호의값.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_S2_괄호의값 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int ans = 0, allSum = 0, sum = 0;
+	
+	public static void main(String[] args) throws Exception {
+		Stack<Character> stk = new Stack<>();
+		
+		char[] str;
+		str = br.readLine().toCharArray();
+		for (int i = 0; i < str.length; i++) {
+			if (stk.isEmpty()) ans += allSum;
+			switch (str[i]){
+			case '(': case '[': stk.push(str[i]); break;
+			case ')': 
+				if ((!stk.isEmpty()) && stk.pop() == '(') {
+					if (allSum == 0) {
+						sum += 2;
+					}
+					 
+					break;
+				}
+				
+			}
+		}
+		
+		int flag = 1;
+		int dummy = Integer.parseInt(br.readLine());
+		String[] bracket = br.readLine().split("");
+
+		for (String s : bracket) {
+			switch (s) {
+//			case "(": case "{": case "[": case "<": stack.push(s); break;
+//			case ")": if (stack.empty() || !stack.pop().​contentEquals("(")) flag = 0; break;
+//			case "]": if (stack.empty() || !stack.pop().contentEquals("[")) flag = 0; break;
+//			case "}": if (stack.empty() || !stack.pop().contentEquals("{")) flag = 0; break;
+//			case ">": if (stack.empty() || !stack.pop().contentEquals("<")) flag = 0; break;
+			}
+		}
+	}
+
+}
