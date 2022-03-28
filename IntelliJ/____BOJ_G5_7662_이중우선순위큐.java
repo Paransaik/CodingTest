9e0ae92901227b6ec4832adf996017640a01ec80
Paransaik <jty419@naver.com>
2022-03-29 01:44:31 +0900

    3.29. BOJ 9205 solving

--- IntelliJ/____BOJ_G5_7662_이중우선순위큐.java
+++ IntelliJ/____BOJ_G5_7662_이중우선순위큐.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ_G5_7662_이중우선순위큐 {
+public class ____BOJ_G5_7662_이중우선순위큐 {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,10 +10,15 @@ public class BOJ_G5_7662_이중우선순위큐 {
         PriorityQueue<Integer> min = new PriorityQueue<>();
         PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
 
+        Stack<Integer> minS = new Stack<>();
+        Stack<Integer> maxS = new Stack<>();
+
         int TC = Integer.parseInt(br.readLine());
         for (int tc = 0; tc < TC; tc++) {
             min.clear();
             max.clear();
+            minS.clear();
+            maxS.clear();
             int N = Integer.parseInt(br.readLine());
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -25,24 +30,21 @@ public class BOJ_G5_7662_이중우선순위큐 {
                         max.add(num);
                         break;
                     case "D":
-                        if (num == -1 && !min.isEmpty()){
-                            ArrayList.remove(min.poll());
-                        }
-                        else if (!max.isEmpty()) {
-                            max.poll();
-//                            min.poll();
+                        if (num == -1 && !min.isEmpty()) {
+                            minS.add(min.poll());
+                        } else if (!max.isEmpty()) {
+                            maxS.add(max.poll());
                         }
                         break;
                 }
-                for (Integer integer : min) {
-                    System.out.print(integer + " ");
+                while (!maxS.isEmpty() && maxS.peek().equals(min.peek())) {
+                    maxS.pop();
+                    min.poll();
                 }
-                System.out.println();
-                for (Integer integer : max) {
-                    System.out.print(integer + " ");
+                while (!minS.isEmpty() && minS.peek().equals(max.peek())) {
+                    minS.pop();
+                    max.poll();
                 }
-                System.out.println();
-                System.out.println();
             }
             if (max.isEmpty() || min.isEmpty()) {
                 bw.append("EMPTY").append("\n");
--- IntelliJ/____BOJ_S1_9205_맥주마시면서걸어가기.java
+++ IntelliJ/____BOJ_S1_9205_맥주마시면서걸어가기.java
@@ -1,17 +1,14 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class ____BOJ_S1_9205_맥주마시면서걸어가기 {
-    static int N, c;
-    static int[][] map;
+    static int N, sY, sX, y, x, eY, eX;
     static String ans;
     static Queue<Position> q = new LinkedList<>();
-
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static final int ALPHA = 32768;
+    static ArrayList<Position> l = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input.txt"));
@@ -22,60 +19,39 @@ public class ____BOJ_S1_9205_맥주마시면서걸어가기 {
         int tc = Integer.parseInt(br.readLine());
         while (tc-- > 0) {
             q.clear();
-            map = new int[1 << 16][1 << 16];
-            N = Integer.parseInt(br.readLine());
-            c = Integer.parseInt(br.readLine());
+            l.clear();
+
+            N = Integer.parseInt(br.readLine()); // 편의점 수
+
             st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken()) + ALPHA;
-            int x = Integer.parseInt(st.nextToken()) + ALPHA;
-            map[y][x] = 1; // start 위치 마킹
-            for (int i = 0; i < c; i++) {
+            sY = Integer.parseInt(st.nextToken());
+            sX = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                map[Integer.parseInt(st.nextToken()) + ALPHA][Integer.parseInt(st.nextToken()) + ALPHA] = 2; // 편의점 위치 마킹
+                y = Integer.parseInt(st.nextToken());
+                x = Integer.parseInt(st.nextToken());
+                l.add(new Position(y, x));
             }
-            map[Integer.parseInt(st.nextToken()) + ALPHA][Integer.parseInt(st.nextToken()) + ALPHA] = 3; // End 위치 마킹
-            ans = bfs(y, x);
-            bw.append(ans).append("\n");
+            st = new StringTokenizer(br.readLine());
+            eY = Integer.parseInt(st.nextToken());
+            eX = Integer.parseInt(st.nextToken());
+
+            bw.append((bfs())? "happy\n": "sad\n");
         }
     }
 
-    public static String bfs(int y, int x) {
-        q.offer(new Position(y, x, 1000));
+    public static boolean bfs() {
 
-        while (!q.isEmpty()) {
-            Position p = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = p.y + dy[i];
-                int nx = p.x + dx[i];
-
-                // 도착 체크
-                if (map[ny][nx] == 3) return "happy"; // y, x 축이 일치하면 true 리턴
-
-                // 50미터 맥주 한캔 캬~
-                p.life -= Math.abs(dx[i]) + Math.abs(dy[i]);
-
-                // 갔는데 편의점이네?
-                if (map[ny][nx] == 2) p.life = 1000;
-                // life 가 0이면 끝
-                if (p.life == 0) continue;
-
-                if (ny < 0 || nx < 0 || ny >= 1 << 16 || nx >= 1 << 16 || map[ny][nx] == 1) continue;
-                map[ny][nx] = 1;
-                q.offer(new Position(ny, nx, p.life));
-            }
-        }
-        return "sad";
+        return false;
     }
 
     public static class Position {
         int y;
         int x;
-        int life;
 
-        public Position(int y, int x, int life) {
+        public Position(int y, int x) {
             this.y = y;
             this.x = x;
-            this.life = life;
         }
     }
 }
