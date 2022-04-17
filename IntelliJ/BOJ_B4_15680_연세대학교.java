bd0839c89e48002673c401a0743642236afede7e
Paransaik <jty419@naver.com>
2022-04-17 19:57:29 +0900

    4.17. BOJ 15680 solved

---
+++ IntelliJ/BOJ_B4_15680_연세대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ_B4_15680_연세대학교 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 0) System.out.println("YONSEI");
+        else System.out.println("Leading the Way to the Future");
+    }
+}
