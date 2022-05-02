25a7ed5367187709eb0537b234852224c91c07ec
정태영 <jty419@naver.com>
2022-05-02 22:59:41 +0900

    5.2. BOJ 2743 solved

---
+++ IntelliJ/BOJ_B2_2743_단어길이재기.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_B2_2743_단어길이재기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
