f1e010c644c79cff1c1fad99af08d6dfcf43c285
정태영 <jty419@naver.com>
2022-05-14 14:39:36 +0900

    5.14. BOJ 10039 solved

---
+++ IntelliJ/BOJ_B4_24883_자동완성.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_B4_24883_자동완성 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        if (a.equals("n") || a.equals("N")) {
+            System.out.println("Naver D2");
+        } else System.out.println("Naver Whale");
+    }
+}
