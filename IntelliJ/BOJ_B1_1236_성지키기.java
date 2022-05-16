2db52638cbe8edf4850be0de57e99238410ff43c
정태영 <jty419@naver.com>
2022-05-16 19:17:31 +0900

    -

--- IntelliJ/BOJ_B1_1236_성지키기.java
+++ IntelliJ/BOJ_B1_1236_성지키기.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_B1_1236_성지키기 {
