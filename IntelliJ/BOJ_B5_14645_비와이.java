4b12ce2a246cbd832c302130268e21a21a6aa8d5
TaeyeongJeong <jty419@naver.com>
2022-05-22 20:08:50 +0900

    5.22. BOJ 14645 solved

---
+++ IntelliJ/BOJ_B5_14645_비와이.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_B5_14645_비와이 {
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+
+          int a = sc.nextInt();
+          sc.nextInt();
+          for (int i = 0; i < a; i++) {
+               sc.next();
+          }
+          System.out.println("비와이");
+     }
+}
