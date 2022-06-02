e2c6e9885bd4b9628aa0b054288ed8228503a1db
TaeyeongJeong <jty419@naver.com>
2022-06-02 16:59:55 +0900

    6.2. BOJ 1844 solving

---
+++ IntelliJ/BOJ_S1_1844_쉬운계단수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BOJ_S1_1844_쉬운계단수 {
+    final static int MOD = 1000000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[][] dp = new int[101][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= a; i++) {
+            for (int j = 0; j < ; j++) {
+                
+            }
+            dp[i]
+            
+        }
+    }
+}
