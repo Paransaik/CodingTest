4e5ae4898752ecb64be7edae9619ff8f8214fd40
정태영 <jty419@naver.com>
2022-06-07 18:40:20 +0900

    6.7. PRO 10821 solving

---
+++ IntelliJ/BOJ_B2_10821_정수의개수.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_B2_10821_정수의개수 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().split(",").length);
+    }
+}
