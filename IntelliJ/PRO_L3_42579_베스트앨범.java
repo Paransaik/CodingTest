4a74527996f7b6e55037ce6fb4a011ecc2736d5e
TaeyeongJeong <jty419@naver.com>
2022-06-18 10:06:22 +0900

    6.18. PRO 42579 solving

--- IntelliJ/PRO_L3_42579_베스트앨범.java
+++ IntelliJ/PRO_L3_42579_베스트앨범.java
@@ -36,14 +36,45 @@ public class PRO_L3_42579_베스트앨범 {
             for (Sing sing : l) {
                 System.out.println(sing);
             }
+            System.out.println();
             Collections.sort(l);
+            for (Sing sing : l) {
+                System.out.println(sing);
+            }
 
-            int[] answer = {};
+            List<Integer> ans = new ArrayList<>();
+            // 2개씩 Ans에 담기
+            String genre = l.get(0).genre;
+            int cnt = 0;
+            boolean pass = false;
+            for (Sing sing : l) {
+
+                if (cnt < 2 && !pass) {
+                    if(genre != sing.genre) continue;
+                    genre = sing.genre;
+                    ans.add(sing.idx);
+                    cnt++;
+                } else if(pass){
+                    continue;
+                }
+//
+//                // Pass가 True면 Pass
+//                if(pass) continue;
+//                if(genre != sing.genre) pass = false;
+//                // 2개를 담았으면 Pass true
+//                if(cnt >= 2) pass = true;
+//
+//                if(cnt < 2 && !pass){
+//                    genre = sing.genre;
+//                    cnt++;
+//                }
+            }
+
             return answer;
         }
     }
 
-    static class Sing implements Comparable{
+    static class Sing implements Comparable<Sing> {
         int idx;
         String genre;
         int cntGenre;
@@ -51,17 +82,20 @@ public class PRO_L3_42579_베스트앨범 {
         int playNo;
 
         public Sing(int idx, String genre, int cntGenre, int sumPlayNo, int playNo) {
-            this.idx = idx;
-            this.genre = genre;
-            this.cntGenre = cntGenre;
-            this.sumPlayNo = sumPlayNo;
-            this.playNo = playNo;
+            this.idx = idx; // 고유번호
+            this.genre = genre; // 장르
+            this.cntGenre = cntGenre; // 장르 개수
+            this.sumPlayNo = sumPlayNo; // 장르 총 플레이 수
+            this.playNo = playNo; // 단일 플레이 수
         }
 
         // 1. 장르 2. 장르내에 재생횟수 3. 고유번호
         @Override
-        public int compareTo(Object o) {
-            return 0;
+        public int compareTo(Sing o) {
+            if (this.cntGenre == o.cntGenre) {
+                return (this.playNo == o.playNo) ? this.idx - o.idx : o.playNo - this.playNo;
+            }
+            return o.cntGenre - this.cntGenre;
         }
 
         @Override
