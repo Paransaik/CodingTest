12026e0079c95c98ed6e3a46e604da65454df734
정태영 <jty419@naver.com>
2022-06-21 18:26:15 +0900

    6.21. PRO 42891 solved

---
+++ IntelliJ/PRO_L4_42891_무지의먹방라이브.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class PRO_L4_42891_무지의먹방라이브 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution(new int[]{1, 1, 1, 1}, 4);
+    }
+
+    static class Solution {
+        // 1. 배열의 최솟값 * 배열 개수를 K에서 뺌
+        // 2. K가 배열의 길이보다 작아질 때까지 반복
+        public int solution(int[] food_times, long k) {
+            int len = food_times.length;
+            LinkedList<int[]> l = new LinkedList<>();
+
+            for (int i = 0; i < len; i++) {
+                l.add(new int[]{i + 1, food_times[i]});
+            }
+            l.sort((Comparator.comparingInt(o -> o[1])));
+
+            int inc = 0;
+            while (k >= len && !l.isEmpty()) {
+                int[] node = l.poll();
+                long minus = (long) (node[1] - inc) * len;
+                if (minus <= k) {
+                    k -= minus;
+                    len--;
+                    inc = node[1];
+                    while (!l.isEmpty() && l.get(0)[1] == inc) {
+                        l.poll();
+                        len--;
+                    }
+                } else {
+                    k -= (k / len) * len;
+                    l.addFirst(node);
+                }
+            }
+            l.sort((Comparator.comparingInt(o -> o[0])));
+
+            if (l.isEmpty()) return -1;
+            else return l.get((int) k)[0];
+        }
+    }
+}
