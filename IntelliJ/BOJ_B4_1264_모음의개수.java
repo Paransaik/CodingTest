677d13ff4f118088859e96484177213b0c06912d
정태영 <jty419@naver.com>
2022-07-03 04:19:52 +0900

    7.3. BOJ 1264 refactoring

--- IntelliJ/BOJ_B4_1264_모음의개수.java
+++ IntelliJ/BOJ_B4_1264_모음의개수.java
@@ -1,20 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.regex.Pattern;
-
+import java.util.*;
 public class BOJ_B4_1264_모음의개수 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String p = "[aeiou]";
-
-        while (true) {
-            char[] word = br.readLine().toLowerCase().toCharArray();
-            if (word[0] == '#') break;
-            int len = word.length, cnt = 0;
-            for (int i = 0; i < len; i++) {
-                if (Pattern.matches(p, String.valueOf(word[i]))) cnt++;
-            }
-            System.out.println(cnt);
+    public static void main(String[] a) {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        while (!"#".equals(s=sc.nextLine())) {
+            System.out.println(s.replaceAll("(?i)[^aeiou]","").length());
         }
     }
 }
