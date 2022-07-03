132d1484ff608774e5023e36da42436a1f2f050e
정태영 <jty419@naver.com>
2022-07-03 21:06:49 +0900

    7.3. BOJ 2738 solved

---
+++ IntelliJ/BOJ_B5_2738_행렬덧셈.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_B5_2738_행렬덧셈 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[][] map = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < c; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < c; j++) {
+                bw.append(String.valueOf(map[i][j] + Integer.parseInt(st.nextToken()))).append(" ");
+            }
+            bw.append("\n");
+        }
+        bw.flush();
+    }
+}
