ec96b842e6ef808336dcbe22ba0b295d80c9e9e6
TaeyeongJeong <jty419@naver.com>
2022-07-27 23:41:41 +0900

    7.27. BOJ 15964 solved

---
+++ IntelliJ/BOJ_S1_1149_RGB거리.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S1_1149_RGB거리 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int size = Integer.parseInt(br.readLine());
+        int[][] dp = new int[size + 1][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < size; j++) {
+                                
+            }
+        }
+    }
+}
